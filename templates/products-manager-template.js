6f7cf5e3701fe875098e5f919e48f5998fb16895
Yuri3358 <yurialmeida917@gmail.com>
2023-01-11 12:58:26 -0300

    refactor: component template in a single file

---
+++ templates/products-manager-template.js
@@ -0,0 +1,36 @@
+const pageTemplate = 
+`<h1>Gerenciador de Produtos</h1>
+<div id="content">
+    <div id="data-entries">
+        <label for="produto">Produto</label>
+        <input class="inputs" id="produto" v-model="productInput"> <br/>
+
+        <label for="quantidade">Quantidade</label>
+        <input class="inputs" id="quantidade" v-model="amountInput"> <br/>
+
+        <button class="btn btn-success" @click="registerProduct">Salvar</button>
+        <button class="btn btn-secondary" @click="updateProduct">Atualizar</button>
+        <button class="btn btn-danger" @click="deleteProduct">Excluir</button>
+
+    </div>
+
+    <div id="data-output">
+        <table class="table table-primary table-striped table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th>Identidade</th>
+                    <th>Produto</th>
+                    <th>Quantidade</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="product in this.products.value">
+                    <td>{{ product.productId }}</td>
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.amount }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>`
